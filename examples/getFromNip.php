c04cff55c2eb0868d4ce13b2d2e636b570d7b7ef
john <john_zuk@wp.pl>
2018-04-21 13:55:42 +0200

    Update example

--- examples/getFromNip.php
+++ examples/getFromNip.php
@@ -8,51 +8,13 @@ use GusApi\GusApi;
 use GusApi\RegonConstantsInterface;
 use GusApi\Exception\InvalidUserKeyException;
 
+//for development test use this:
+$key = 'abcde12345abcde12345';
+$gus = new GusApi($key, 'dev');
 
-$key = 'abcde12345abcde12345'; // <--- your user key / twój klucz użytkownika
-
-$context = new \GusApi\Context\Context();
-$soap = new \GusApi\Client\SoapClient(
-    RegonConstantsInterface::BASE_WSDL_URL_TEST,
-    [
-        'soap_version' => SOAP_1_2,
-        'trace' => true,
-        'style' => SOAP_DOCUMENT,
-        'stream_context' => $context->getContext(),
-        'classmap' => [
-            'ZalogujResponse' => \GusApi\Type\LoginResponse::class,
-            'WylogujResponse' => \GusApi\Type\LogoutResponse::class,
-            'GetValueResponse' => \GusApi\Type\GetValueResponse::class,
-            'DaneSzukajResponse' => \GusApi\Type\SearchResponseRaw::class,
-            'DanePobierzPelnyRaportResponse' => \GusApi\Type\GetFullReportResponseRaw::class
-        ]
-    ]
-);
-$client = new \GusApi\Client\GusApiClient($soap, RegonConstantsInterface::BASE_WSDL_ADDRESS_TEST, $context);
-
-var_dump($sid = $client->login(new \GusApi\Type\Login('abcde12345abcde12345'))->ZalogujResult);
-var_dump($client->getValue(
-    new \GusApi\Type\GetValue(RegonConstantsInterface::PARAM_STATUS_DATE_STATE), $sid
-));
-
-$params = new \GusApi\Type\SearchParameters();
-$params->setNip('8951930748');
-$search = new \GusApi\Type\SearchData($params);
-var_dump($client->searchData($search, $sid));
-
-$searchData = new \GusApi\Type\GetFullReport('02083251200000', 'PublDaneRaportTypJednostki');
-var_dump($client->getFullReport($searchData, $sid));
-exit();
-
-$gus = new GusApi(
-    $key,
-    new \GusApi\Adapter\Soap\SoapAdapter(
-        RegonConstantsInterface::BASE_WSDL_URL_TEST,
-        RegonConstantsInterface::BASE_WSDL_ADDRESS_TEST //<--- production server / serwer produkcyjny
-    //for test serwer use RegonConstantsInterface::BASE_WSDL_ADDRESS_TEST
-    //w przypadku serwera testowego użyj: RegonConstantsInterface::BASE_WSDL_ADDRESS_TEST
-    )
-);
+//for production use this:
+//$key = 'your_api_key';
+//$gus = new GusApi($key);
 
 if (isset($_GET['reset'])) {
     $_SESSION = [];
@@ -63,8 +25,10 @@ if ($gus->serviceStatus() === RegonConstantsInterface::SERVICE_AVAILABLE) {
 
     try {
 
-        if (!isset($_SESSION['sid']) || !$gus->isLogged($_SESSION['sid'])) {
+        if (!isset($_SESSION['sid']) || !$gus->isLogged()) {
             $_SESSION['sid'] = $gus->login();
+        } else {
+            $gus->setSessionId($_SESSION['id']);
         }
 
         printNipForm();
@@ -73,7 +37,7 @@ if ($gus->serviceStatus() === RegonConstantsInterface::SERVICE_AVAILABLE) {
 
             $nip = $_POST['nip'];
             try {
-                $gusReports = $gus->getByNip($_SESSION['sid'], $nip);
+                $gusReports = $gus->getByNip($nip);
                 var_dump($gusReports);
                 $mapper = new \GusApi\ReportTypeMapper();
 
@@ -81,7 +45,6 @@ if ($gus->serviceStatus() === RegonConstantsInterface::SERVICE_AVAILABLE) {
                     $reportType = $mapper->getReportType($gusReport);
 
                     var_dump($gus->getFullReport(
-                        $_SESSION['sid'],
                         $gusReport,
                         $reportType
                     ));
@@ -92,7 +55,7 @@ if ($gus->serviceStatus() === RegonConstantsInterface::SERVICE_AVAILABLE) {
             } catch (\GusApi\Exception\NotFoundException $e) {
                 echo 'No data found <br>';
                 echo 'For more information read server message belowe: <br>';
-                echo $gus->getResultSearchMessage($_SESSION['sid']);
+                echo $gus->getResultSearchMessage();
 
             }
         }
