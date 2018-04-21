346b5aa5fe4a565482541d2aaa784176006d24c3
john <john_zuk@wp.pl>
2018-04-21 14:19:39 +0200

    Add example

--- src/GusApi/ReportTypes.php
+++ src/GusApi/ReportTypes.php
@@ -24,4 +24,23 @@ final class ReportTypes
     public const REPORT_ACTIVITY_LOCAL_LAW_PUBLIC = 'PublDaneRaportDzialalnosciLokalnejPrawnej';
     public const REPORT_COMMON_LAW_PUBLIC = 'PublDaneRaportWspolnicyPrawnej';
     public const REPORT_UNIT_TYPE_PUBLIC = 'PublDaneRaportTypJednostki';
+
+    public const REPORTS = [
+        self::REPORT_ACTIVITY_PHYSIC_PERSON,
+        self::REPORT_ACTIVITY_PHYSIC_CEIDG,
+        self::REPORT_ACTIVITY_PHYSIC_AGRO,
+        self::REPORT_ACTIVITY_PHYSIC_OTHER_PUBLIC,
+        self::REPORT_ACTIVITY_LOCAL_PHYSIC_WKR_PUBLIC,
+        self::REPORT_LOCALS_PHYSIC_PUBLIC,
+        self::REPORT_LOCAL_PHYSIC_PUBLIC,
+        self::REPORT_ACTIVITY_PHYSIC_PUBLIC,
+        self::REPORT_ACTIVITY_LOCAL_PHYSIC_PUBLIC,
+        self::REPORT_PUBLIC_LAW,
+        self::REPORT_ACTIVITY_LAW_PUBLIC,
+        self::REPORT_LOCALS_LAW_PUBLIC,
+        self::REPORT_LOCAL_LAW_PUBLIC,
+        self::REPORT_ACTIVITY_LOCAL_LAW_PUBLIC,
+        self::REPORT_COMMON_LAW_PUBLIC,
+        self::REPORT_UNIT_TYPE_PUBLIC
+    ];
 }
