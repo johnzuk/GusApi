f296757e4f990c6ed797f836a13aec38201bc4ac
john <john_zuk@wp.pl>
2018-04-21 13:45:57 +0200

    Add gus api tests

--- tests/GusApiTest.php
+++ tests/GusApiTest.php
@@ -2,13 +2,57 @@
 
 namespace GusApi\Tests;
 
+use GusApi\Client\GusApiClient;
 use GusApi\GusApi;
+use GusApi\Type\Login;
+use GusApi\Type\LoginResponse;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class GusApiTest extends TestCase
 {
+    protected $userKey = '123absdefg123';
+
+    /**
+     * @var MockObject
+     */
+    protected $apiClient;
+
+    /**
+     * @var GusApi
+     */
+    protected $api;
+
+    public function setUp()
+    {
+        $this->apiClient = $this->createMock(GusApiClient::class);
+        $this->api = GusApi::createWithApiClient($this->userKey, $this->apiClient);
+    }
+
     public function testLogin()
     {
+        $this->apiClient
+            ->method('login')
+            ->with(
+                $this->equalTo(new Login($this->userKey))
+            )
+            ->willReturn(new LoginResponse('12sessionid21'));
 
+        $this->assertSame('12sessionid21', $this->api->login());
+    }
+
+    /**
+     * @expectedException \GusApi\Exception\InvalidUserKeyException
+     */
+    public function testInvalidLogin()
+    {
+        $this->apiClient
+            ->method('login')
+            ->with(
+                $this->equalTo(new Login($this->userKey))
+            )
+            ->willReturn(new LoginResponse(''));
+
+        $this->api->login();
     }
 }
